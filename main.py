9e3b67ba5db2dd73a0e3a27b92d4b0510df6c0bb
mahdifire1403 <dft.ahadi@gmail.com>
2024-12-04 23:24:10 +0330

    generate code and catch number from user

---
+++ main.py
@@ -0,0 +1,8 @@
+import redis
+import random
+#generate random code
+random_Code=random.randint(1 , 10)
+mobile_number=""
+#catch mobile number from user
+mobile_number=input(f'please enter your mobile number:{mobile_number}')
+print (mobile_number)
